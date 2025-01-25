20ae3c92fab7b756c5d860f5bd4fb69ccde903d8
printCherta <chavezaldair01@gmail.com>
2025-01-25 00:12:10 -0600

    Update SvUsuarios.java

--- FirstServlet/src/main/java/com/aldairdev/firstservlet/servlets/SvUsuarios.java
+++ FirstServlet/src/main/java/com/aldairdev/firstservlet/servlets/SvUsuarios.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author ALDAIR CHAVEZ
+ * @author ALDAIR CHAVEZ prueba git
  */
 @WebServlet(name = "SvUsuarios", urlPatterns = {"/SvUsuarios"})
 public class SvUsuarios extends HttpServlet {
