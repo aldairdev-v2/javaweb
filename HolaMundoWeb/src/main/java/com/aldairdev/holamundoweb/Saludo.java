ba444e012b564144398ca435a9a6edaf52dc047c
printCherta <chavezaldair01@gmail.com>
2025-01-25 14:11:12 -0600

    25/01/25

--- HolaMundoWeb/src/main/java/com/aldairdev/holamundoweb/Saludo.java
+++ HolaMundoWeb/src/main/java/com/aldairdev/holamundoweb/Saludo.java
@@ -2,22 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package com.aldairdev.firstservlet.servlets;
+package com.aldairdev.holamundoweb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author ALDAIR CHAVEZ prueba git
+ * @author CHAVEZ
  */
-@WebServlet(name = "SvUsuarios", urlPatterns = {"/SvUsuarios"})
-public class SvUsuarios extends HttpServlet {
+public class Saludo extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,18 +30,18 @@ public class SvUsuarios extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. 
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SvUsuarios</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SvUsuarios at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-            */
-            out.println("Hola mundo!!");
+            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet Saludo</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet Saludo at " + request.getContextPath() + "</h1>");
+//            out.println("<h1>Alan Aldair Chávez Hernández</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+ out.println("hola mundo");
         }
     }
 
