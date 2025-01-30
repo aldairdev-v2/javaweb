7e70a6f63f168205b2a1d09fe52e64f15a7bf90b
printCherta <chavezaldair01@gmail.com>
2025-01-30 10:15:27 -0600

    java bd

---
+++ JavaBD/src/main/java/com/aldairdev/javabd/Conexion.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aldairdev.javabd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    
+    Connection conectar = null;
+    
+    String usuario ="postgres";
+    String contrasenia = "admin";
+    String bd = "alquiler_a";
+    String ip = "localhost";
+    String puerto = "5432";
+    
+    String cadena = "jdbc:postgresql://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection establecerConexion(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            conectar = DriverManager.getConnection(cadena,usuario,contrasenia);
+            JOptionPane.showMessageDialog(null, "Conexión exitosa a la BD");
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar a la BD: " +e.toString());
+        }
+        
+        return conectar;
+    }
+    
+}
---
+++ JavaBD/src/main/java/com/aldairdev/javabd/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aldairdev.javabd;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Conexion objConexion = new Conexion();
+        objConexion.establecerConexion();  
+    }
+}
