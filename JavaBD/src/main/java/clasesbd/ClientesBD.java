654ea6d7cb3064f47d00d386e5cc611aa97661a2
printCherta <chavezaldair01@gmail.com>
2025-02-08 14:36:05 -0600

    bd

--- JavaBD/src/main/java/clasesbd/ClientesBD.java
+++ JavaBD/src/main/java/clasesbd/ClientesBD.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package clasesbd;
 
-/**
- *
- * @author CHAVEZ
- */
 public class ClientesBD {
     private String nomcliente;
     private String apppatcliente;
