0752ad2ce12980542ebc13e07ef12fe26a610bf1
printCherta <chavezaldair01@gmail.com>
2025-02-14 20:33:44 -0600

    practica arraylist
    retroalimentación

---
+++ Retroalimentacionbasica/src/main/java/com/mycompany/retroalimentacionbasica/Retroalimentacionbasica.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.retroalimentacionbasica;
+
+public class Retroalimentacionbasica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Retroalimentacionbasica/src/main/java/retroalimentacionbasica/pedido/Pedido.java
@@ -0,0 +1,47 @@
+
+package retroalimentacionbasica.pedido;
+
+
+public class Pedido {
+    
+    private String nombrepedido;
+    private int cantidad;
+    private String marca;
+    private String tienda;
+
+    public String getNombrepedido() {
+        return nombrepedido;
+    }
+
+    public void setNombrepedido(String nombrepedido) {
+        this.nombrepedido = nombrepedido;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTienda() {
+        return tienda;
+    }
+
+    public void setTienda(String tienda) {
+        this.tienda = tienda;
+    }
+    
+    
+     
+    
+}
